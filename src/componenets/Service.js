347aff95b5d971aa294260d29a4511a30500d57d
Swetha Sakthi <swethasakthi927@gmail.com>
2023-02-07 18:48:34 +0530

    commit

--- src/componenets/Service.js
+++ src/componenets/Service.js
@@ -147,7 +147,7 @@ const Services = () => {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h4" className={classes.title}>
-            service Management
+            Service Management
           </Typography>
 
           <div className={classes.navLinks}>
